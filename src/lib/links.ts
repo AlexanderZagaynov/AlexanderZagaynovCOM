474547dff7e3b1c9316a0b0f521ccb17d35b7a42
Alexander Zagaynov <zalex80@gmail.com>
2026-07-07 08:01:46 +0200

    Update CV

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,7 +1,7 @@
 export const chromeWebStoreUrl =
   'https://chromewebstore.google.com/detail/latertabs/ebnchiihpebnopjlneinbeeanljpeahk'
 
-export const cvPdfPath = '/assets/cv/alexander-zagaynov-cv-2026-04.pdf'
+export const cvPdfPath = '/assets/cv/alexander-zagaynov-cv-2026-07.pdf'
 
 export const contactEmail = 'zalex80@gmail.com'
 export const linkedInUrl = 'https://www.linkedin.com/in/AlexanderZagaynov'
